f1595192ffe50c52d8e41762e07aa81c9f0b818b
Christian Gräfe <chr.graefe@gmail.com>
2017-10-11 10:54:16 +0200

    Flexible time selection without hardcoded string

--- Monitoring/IdentifyTheSlowestQueries.sql
+++ Monitoring/IdentifyTheSlowestQueries.sql
@@ -2,26 +2,26 @@ SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 
 WITH  cteQueries AS
 (
-SELECT 
---TOP 150
- CAST(qs.total_elapsed_time / 1000000.0 AS NUMERIC(28, 2)) AS [Total Elapsed Duration (sec)]
-,qs.execution_count
+	SELECT 
+	--TOP 150
+	CAST(qs.total_elapsed_time / 1000000.0 AS NUMERIC(28, 2)) AS [Total Elapsed Duration (sec)]
+	,qs.execution_count
 
-,SUBSTRING(qt.text, (qs.statement_start_offset / 2) + 1, ((CASE WHEN qs.statement_end_offset = -1 
-															THEN LEN(CONVERT(NVARCHAR(MAX), qt.text)) * 2
-															ELSE qs.statement_end_offset
-															END - qs.statement_start_offset) / 2) + 1) AS [Individual Query]
+	,SUBSTRING(qt.text, (qs.statement_start_offset / 2) + 1, ((CASE WHEN qs.statement_end_offset = -1 
+																THEN LEN(CONVERT(NVARCHAR(MAX), qt.text)) * 2
+																ELSE qs.statement_end_offset
+																END - qs.statement_start_offset) / 2) + 1) AS [Individual Query]
 
-,qt.text [Parent Query]
-,DB_NAME(qt.dbid) AS DatabaseName
-,qp.query_plan
-FROM sys.dm_exec_query_stats qs
-CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
-CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
-WHERE 
-	qs.last_execution_time > '27.09.2011 00:00:00'
+	,qt.text [Parent Query]
+	,DB_NAME(qt.dbid) AS DatabaseName
+	,qp.query_plan
+	FROM sys.dm_exec_query_stats qs
+	CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
+	CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
+	WHERE 
+		qs.last_execution_time > DATEADD(DD, -2, GETDATE())
 
---order by qs.total_elapsed_time desc
+	--order by qs.total_elapsed_time desc
 )
 select
  SUM([Total Elapsed Duration (sec)]) [Total Elapsed Duration (sec)]
@@ -32,9 +32,9 @@ select
 ,[Parent Query]
 from cteQueries cte
 WHERE
---	cte.[Parent Query] LIKE '%SP_Wagensuche_Result%' 
-DatabaseName = 'ALF'
+	DatabaseName = DB_NAME(DB_ID())
 GROUP BY
-[Parent Query]
-	
-ORDER BY [Total Elapsed Duration (sec)] ASC
+	[Parent Query]
+ORDER BY 
+	[Total Elapsed Duration (sec)] ASC
+;
