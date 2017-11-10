aecda8cceb496798666ec078a480e1dc1f72f797
Christian Gräfe <chr.graefe@gmail.com>
2017-11-10 11:58:09 +0100

    Simplified installing FRK WhoIsActive and MaintenanceSolution incl. schedules

--- PowerShell/BlitzSql/Install-Blitz.ps1
+++ PowerShell/BlitzSql/Install-Blitz.ps1
@@ -1,6 +1,6 @@
 param (
-    [Parameter(Mandatory=$true)]
-    [String[]] $Instances
+    [Parameter(Mandatory=$false)]
+    [String[]] $Instances = "localhost\SQL2017"
 )
 
 Import-Module dbatools;
@@ -36,15 +36,48 @@ function Set-DatabaseIfNotExists
 #exec sp_BlitzCache @SortOrder = 'cpu'
 #
 
-Get-ChildItem ".\" -Filter *.sql | 
-Foreach-Object {
-    $_.FullName
+$jobCmdCleanup = "CommandLog Cleanup"
+$jobSystemDbFull = "DatabaseBackup - SYSTEM_DATABASES - FULL"
+$jobUserDbDiff = "DatabaseBackup - USER_DATABASES - DIFF"
+$jobUserDbFull = "DatabaseBackup - USER_DATABASES - FULL"
+$jobUserDbLog = "DatabaseBackup - USER_DATABASES - LOG"
+$jobSystemDbIntegrity = "DatabaseIntegrityCheck - SYSTEM_DATABASES"
+$jobUserDbIntegrity = "DatabaseIntegrityCheck - USER_DATABASES"
+$jobUserDbFullIndexOptimize = "IndexOptimize - USER_DATABASES"
+$jobOutFileCleanup = "Output File Cleanup"
+$jobDeleteBackupHistory = "sp_delete_backuphistory"
+$jobPurgeJobHistory = "sp_purge_jobhistory"
+
+#Get-ChildItem ".\" -Filter *.sql | 
+#Foreach-Object {
+ #   $_.FullName
     Foreach ($instance in $instances) 
     {
-        Set-DatabaseIfNotExists -SqlServer $instance -DatabaseName "DBA";
+        #Set-DatabaseIfNotExists -SqlServer $instance -DatabaseName "DBA";        
+        #Install-DbaFirstResponderKit -SqlInstance $instance -Database "DBA"
+        #Install-DbaWhoIsActive -SqlInstance $instance -Database "DBA"
+        Install-DbaMaintenanceSolution -SqlInstance $instance -Database "DBA" -BackupLocation "C:\TEMP\" -CleanupTime 2400 -InstallJobs -ReplaceExisting
 
-        Invoke-Sqlcmd -ServerInstance $instance -Database "DBA" -InputFile $_.FullName;
+
+        # DatabaseBackup - USER_DATABASES - FULL
+        Remove-DbaAgentSchedule -SqlInstance $instance -Schedule "schedule_$jobUserDbFull" -Force
+        New-DbaAgentSchedule -SqlInstance $instance -Job $jobUserDbFull -Schedule "schedule_$jobUserDbFull" -FrequencyType Weekly -FrequencyInterval Sunday -FrequencyRecurrenceFactor 1 -StartDate "20170101" -StartTime "000000" -EndDate "20991231" -EndTime "235959"
+
+        # DatabaseBackup - USER_DATABASES - FULL
+        Remove-DbaAgentSchedule -SqlInstance $instance -Schedule "schedule_$jobUserDbDiff" -Force
+        New-DbaAgentSchedule -SqlInstance $instance -Job $jobUserDbDiff -Schedule "schedule_$jobUserDbDiff" -FrequencyType Weekly -FrequencyInterval Monday,Tuesday,Wednesday,Thursday,Friday,Saturday -FrequencyRecurrenceFactor 1 -StartDate "20170101" -StartTime "000000" -EndDate "20991231" -EndTime "235959"
+        
+        # DatabaseBackup - USER_DATABASES - LOG
+        Remove-DbaAgentSchedule -SqlInstance $instance -Schedule "schedule_$jobUserDbLog" -Force
+        New-DbaAgentSchedule -SqlInstance $instance -Job $jobUserDbLog -Schedule "schedule_$jobUserDbLog" -FrequencyType Daily -FrequencyInterval EveryDay -FrequencySubdayType Minutes -FrequencySubdayInterval 15 -StartDate "20170101" -StartTime "000000" -EndDate "20991231" -EndTime "235959"
+        
+        #Invoke-Sqlcmd -ServerInstance $instance -Database "DBA" -InputFile $_.FullName;
         #Install-SqlWhoIsActive -SqlServer $instance;
     }
-}
+#}
+
+
+
+
+
 
