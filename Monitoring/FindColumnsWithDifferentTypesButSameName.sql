b61abdffa416f27c53760425ad54cbc420948604
Christian Gräfe <chr.graefe@gmail.com>
2018-02-15 09:27:25 +0100

    extend to computed columns

--- Monitoring/FindColumnsWithDifferentTypesButSameName.sql
+++ Monitoring/FindColumnsWithDifferentTypesButSameName.sql
@@ -16,6 +16,22 @@ WITH allColumnsOfDatabase AS
 
     UNION
 
+    /* get all computed columns of tables, views and table-value-functions */
+    SELECT 
+    sc.name ColumnName
+    ,case
+        when sysType.name IN (N'char', N'nchar', N'varchar', N'nvarchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length as varchar(5))) +')'
+        when sysType.name IN (N'decimal') then sysType.name+'('+ cast(sc.precision as varchar(2)) + ', ' + cast(sc.scale as varchar(2)) + ')'
+        else sysType.name
+    end TypeSyntax
+    FROM sys.computed_columns sc
+    INNER JOIN sys.types sysType
+        ON sysType.user_type_id = sc.user_type_id
+    WHERE
+        EXISTS (SELECT 1 FROM sys.objects so WHERE so.object_id = sc.object_id AND so.is_ms_shipped = 0)
+
+    UNION
+
     /* get all parameters of functions and procedures */
     SELECT 
     CASE
@@ -66,6 +82,30 @@ select
 end TypeSyntax
 from columnsWithDifferentTypesButSameName cte3
 inner join sys.columns sc 
+    ON sc.name = cte3.ColumnName
+INNER JOIN sys.types sysType
+    ON sysType.user_type_id = sc.user_type_id
+INNER JOIN sys.objects so 
+    ON so.object_id = sc.object_id
+WHERE
+    so.is_ms_shipped = CAST(0 as BIT)
+
+UNION
+
+select 
+ cte3.ColumnName
+, 'Computed Column' TypeOf
+,SCHEMA_NAME(so.schema_id) ObjectSchema
+,so.name ObjectName
+,so.type_desc ObjectDescription
+,case
+    when sysType.name IN (N'char', N'varchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length as varchar(5))) +')'
+    when sysType.name IN (N'nchar', N'nvarchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length/2 as varchar(5))) +')'
+    when sysType.name IN (N'decimal') then sysType.name+'('+ cast(sc.precision as varchar(2)) + ', ' + cast(sc.scale as varchar(2)) + ')'
+    else sysType.name
+end TypeSyntax
+from columnsWithDifferentTypesButSameName cte3
+inner join sys.computed_columns sc 
     ON sc.name = cte3.ColumnName
 INNER JOIN sys.types sysType
     ON sysType.user_type_id = sc.user_type_id
