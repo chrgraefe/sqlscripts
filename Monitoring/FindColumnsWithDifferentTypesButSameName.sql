6741805111c07e899985b2a33b0ee27c47f313c3
Christian Gräfe <chr.graefe@gmail.com>
2018-02-15 09:15:08 +0100

    NVARCHAR columns mit N'' versehen

--- Monitoring/FindColumnsWithDifferentTypesButSameName.sql
+++ Monitoring/FindColumnsWithDifferentTypesButSameName.sql
@@ -4,8 +4,8 @@ WITH allColumnsOfDatabase AS
     SELECT 
     sc.name ColumnName
     ,case
-        when sysType.name IN ('char', 'nchar', 'varchar', 'nvarchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length as varchar(5))) +')'
-        when sysType.name IN ('decimal') then sysType.name+'('+ cast(sc.precision as varchar(2)) + ', ' + cast(sc.scale as varchar(2)) + ')'
+        when sysType.name IN (N'char', N'nchar', N'varchar', N'nvarchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length as varchar(5))) +')'
+        when sysType.name IN (N'decimal') then sysType.name+'('+ cast(sc.precision as varchar(2)) + ', ' + cast(sc.scale as varchar(2)) + ')'
         else sysType.name
     end TypeSyntax
     FROM sys.columns sc
@@ -19,12 +19,12 @@ WITH allColumnsOfDatabase AS
     /* get all parameters of functions and procedures */
     SELECT 
     CASE
-        WHEN param.Name = '' THEN 'ReturnValueOfScalarFunction'
+        WHEN param.Name = N'' THEN N'ReturnValueOfScalarFunction'
         ELSE param.Name
      END ColumnName
     ,CASE
-            when sysType.name IN ('char', 'nchar', 'varchar', 'nvarchar') then sysType.name+'('+IIF(param.max_length=-1, 'max' ,cast(param.max_length as varchar(5))) +')'
-            when sysType.name IN ('decimal') then sysType.name+'('+ cast(param.precision as varchar(2)) + ', ' + cast(param.scale as varchar(2)) + ')'
+            when sysType.name IN (N'char', N'nchar', N'varchar', N'nvarchar') then sysType.name+'('+IIF(param.max_length=-1, 'max' ,cast(param.max_length as varchar(5))) +')'
+            when sysType.name IN (N'decimal') then sysType.name+'('+ cast(param.precision as varchar(2)) + ', ' + cast(param.scale as varchar(2)) + ')'
             else sysType.name
         end TypeSyntax
     from sys.parameters param
@@ -59,9 +59,9 @@ select
 ,so.name ObjectName
 ,so.type_desc ObjectDescription
 ,case
-    when sysType.name IN ('char', 'varchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length as varchar(5))) +')'
-    when sysType.name IN ('nchar', 'nvarchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length/2 as varchar(5))) +')'
-    when sysType.name IN ('decimal') then sysType.name+'('+ cast(sc.precision as varchar(2)) + ', ' + cast(sc.scale as varchar(2)) + ')'
+    when sysType.name IN (N'char', N'varchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length as varchar(5))) +')'
+    when sysType.name IN (N'nchar', N'nvarchar') then sysType.name+'('+IIF(sc.max_length=-1, 'max' ,cast(sc.max_length/2 as varchar(5))) +')'
+    when sysType.name IN (N'decimal') then sysType.name+'('+ cast(sc.precision as varchar(2)) + ', ' + cast(sc.scale as varchar(2)) + ')'
     else sysType.name
 end TypeSyntax
 from columnsWithDifferentTypesButSameName cte3
@@ -72,27 +72,27 @@ INNER JOIN sys.types sysType
 INNER JOIN sys.objects so 
     ON so.object_id = sc.object_id
 WHERE
-    so.is_ms_shipped = 0
+    so.is_ms_shipped = CAST(0 as BIT)
 
 UNION
 
 select 
  CASE
-    WHEN param.Name = '' THEN 'ReturnValueOfScalarFunction'
-    ELSE REPLACE(param.Name, '@', '')
+    WHEN param.Name = N'' THEN N'ReturnValueOfScalarFunction'
+    ELSE REPLACE(param.Name, N'@', N'')
  END ColumnName
 , 'Parameter' TypeOf
 , SCHEMA_NAME(so.schema_id) ObjectSchema
 , so.name ObjectName
 , so.type_desc ObjectDescription
 , case
-        when sysType.name IN ('char', 'nchar', 'varchar', 'nvarchar') then sysType.name+'('+IIF(param.max_length=-1, 'max' ,cast(param.max_length as varchar(5))) +')'
-        when sysType.name IN ('decimal') then sysType.name+'('+ cast(param.precision as varchar(2)) + ', ' + cast(param.scale as varchar(2)) + ')'
+        when sysType.name IN (N'char', N'nchar', N'varchar', N'nvarchar') then sysType.name+'('+IIF(param.max_length=-1, 'max' ,cast(param.max_length as varchar(5))) +')'
+        when sysType.name IN (N'decimal') then sysType.name+'('+ cast(param.precision as varchar(2)) + ', ' + cast(param.scale as varchar(2)) + ')'
         else sysType.name
     end TypeSyntax
 from columnsWithDifferentTypesButSameName cte3
 INNER JOIN sys.parameters param
-    ON cte3.ColumnName = CASE WHEN param.Name = '' THEN 'ReturnValueOfScalarFunction' ELSE REPLACE(param.Name, '@', '') END
+    ON cte3.ColumnName = CASE WHEN param.Name = N'' THEN N'ReturnValueOfScalarFunction' ELSE REPLACE(param.Name, N'@', N'') END
 inner join sys.objects so 
     ON so.object_id = param.object_id
 INNER JOIN sys.types sysType
